adbbbb035f1c454d6bdbf119255aa056a76a94dc
Sijianf <42521033+Sijianf@users.noreply.github.com>
2025-12-31 22:46:41 +0000

    Deploying to gh-pages from @ Sijianf/sijianf.github.io@532566dcd660855b335d164500c0b4e6bdeded45 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -497,13 +497,6 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
@@ -516,13 +509,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://github.com/sijianf", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',

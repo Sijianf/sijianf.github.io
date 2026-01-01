9b3ac4aef1c73daeab3bc33b940f4517ed74faca
Sijianf <42521033+Sijianf@users.noreply.github.com>
2026-01-01 04:59:21 +0000

    Deploying to gh-pages from @ Sijianf/sijianf.github.io@eea8285657c357495d8ed0c5797d1c6530486590 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,6 +511,13 @@ ninja.data = [{
           window.open("https://github.com/sijianf", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/sijian-fan-1902b01a8", "_blank");
+        },
+      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
